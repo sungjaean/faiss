321774c27d2b89ff8921ce9da4030aed0088240e
sungjaean <sungjaean@sogang.ac.kr>
2025-07-30 14:41:33 +0900

    new file:   hnswlib1/main.py

---
+++ hnswlib1/main.py
@@ -0,0 +1,26 @@
+import hnswlib
+import numpy as np
+
+
+dim = 256   #dimension of vectors
+num_elements = 10000    #number of vectors
+data = np.random.rand(num_elements,dim).astype(np.float32)  #example data
+
+
+# index 
+
+p = hnswlib.Index(space = 'l2', dim = dim) 
+
+
+#index initialize
+p.init_index(max_elements=num_elements, ef_construction=200, M = 16)
+p.set_ef(200)
+
+p.add_items(data)
+
+query_vector = np.random.rand(dim).astype(np.float32)
+
+labels, distances = p.knn_query(query_vector,k = 5)
+
+print("Nearest Neighbors'   labels: ", labels)
+print("Distances: ", distances)
